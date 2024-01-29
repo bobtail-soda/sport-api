3d552e38714aa76400a3e0aa8afb92acec8ddcfd
SenPatthamawan <sen.patthamwan@gmail.com>
2024-01-30 01:17:27 +0700

    feat: Create API Exercise Activity [GET] and [POST]

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,15 @@
+import clc from 'cli-color';
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import ViteExpress from 'vite-express';
 import { config } from './config/config.js';
 import mongo from './database/db.js';
+import userController from './modules/users/user.controller.js';
 import userRouter from './modules/users/user.router.js';
+import exerciseActivityRouter from './modules/exerciseActivity/exerciseActivity.router.js'
 
 console.log({ config });
 
@@ -13,9 +18,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRouter);
+app.use('/api/exercise-activities', exerciseActivityRouter);
 
 app.get('/hello', (req, res) => {
   res.send('Hello Vite!');
 });
-mongo();
-ViteExpress.listen(app, config.port, () => console.log(`Server is listening on port ${config.port}...`));
+
+mongo(); // To test and for connected with mongoDB
+ViteExpress.listen(app, config.port, () => console.log(`Server is listening on port ${clc.yellow(config.port)}...`));
