1cef1955e94355fd436cb7a8dc6969ac74a18f1c
ekachai39_8re <146946429+ekachaikeaw@users.noreply.github.com>
2024-02-02 10:51:15 +0700

    🔧 change config path on user.auth.js

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,6 +1,7 @@
 export const config = {
   port: Number(process.env.PORT ?? 3000),
   mongodbUri: process.env.MONGODB_URI,
+  jwt_secret_key: process.env.JWT_SECRET_KEY,
 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
