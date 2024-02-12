66f83670e72aca52f71a2d12834c93c8a1d008e1
ekachai39_8re <146946429+ekachaikeaw@users.noreply.github.com>
2024-02-12 17:25:48 +0700

    somthing

--- src/server/modules/users/user.router.js
+++ src/server/modules/users/user.router.js
@@ -8,7 +8,7 @@ router.post('/', userController.createUser);
 router.get('/', auth, userController.getUsers);
 router.get('/:id', auth, userController.getUserById);
 
-router.put('/:id', auth, userController.updateUser);
+router.post('/:id', auth, userController.updateUser);
 router.patch('/:id/change-password', auth, userController.changePassword);
 router.delete('/:id', auth, userController.deleteUser);
 router.patch('/photo/:id', uploadSingle, uploadToCloudinary, updatePhotoByUserID);
