56d7e877a64f90c94be474123577dcd67d46567a
SenPatthamawan <sen.patthamwan@gmail.com>
2024-02-15 13:14:38 +0700

    Plan to get by date_range

--- src/server/modules/dashboard/dashboard.controller.js
+++ src/server/modules/dashboard/dashboard.controller.js
@@ -2,8 +2,42 @@ import mongoose from 'mongoose';
 import userModel from '../users/user.model.js';
 
 const getsummaryCardByUserId = async (req, res) => {
+  // #swagger.tags = ['Dashboard']
+
   try {
     const { user_id } = req.params;
+    const { range } = req.query;
+    let startDate, endDate;
+    switch (range) {
+      case 'today':
+        // ช่วงรายชั่วโมง
+        startDate = new Date();
+        endDate = new Date();
+        startDate.setHours(startDate.getHours() - 1);
+        break;
+      case 'weekly':
+        // ช่วงย้อนหลัง 7 วัน
+        endDate = new Date();
+        startDate = new Date(endDate);
+        startDate.setDate(startDate.getDate() - 6);
+        break;
+      case 'monthly':
+        // ช่วงรายเดือน
+        const today = new Date();
+        startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        break;
+      case 'yearly':
+        // ช่วงรายปี
+        const currentYear = new Date().getFullYear();
+        startDate = new Date(currentYear, 0, 1);
+        endDate = new Date(currentYear, 11, 31);
+        break;
+      default:
+        // ค่าเริ่มต้นหากไม่ระบุ range
+        startDate = null;
+        endDate = null;
+    }
 
     const result = await userModel.aggregate([
       {
@@ -19,6 +53,9 @@ const getsummaryCardByUserId = async (req, res) => {
       },
       {
         $unwind: '$activities',
+      },
+      {
+        $match: { 'activities.date': { $gte: startDate, $lte: endDate } }, // Filter activities within the specified date range
       },
       {
         $group: {
@@ -161,7 +198,7 @@ const getGraphSummaryDataByUserId = async (req, res) => {
           count: { $sum: 1 },
           totalCalories: { $sum: '$activities.calories' },
           totalDuration: { $sum: { $add: [{ $multiply: ['$activities.hour', 60] }, '$activities.minute'] } },
-          totalDistance: { $sum: '$activities.distance' }
+          totalDistance: { $sum: '$activities.distance' },
         },
       },
       {
@@ -170,21 +207,21 @@ const getGraphSummaryDataByUserId = async (req, res) => {
           activity_type_name: '$_id',
           calories: '$totalCalories',
           duration: { $divide: ['$totalDuration', 60] }, // Convert total duration to hours
-          distance: '$totalDistance'
+          distance: '$totalDistance',
         },
       },
     ]);
 
     // Organize data for response
     const series = [
-      { name: 'Calories', data: result.map(activity => activity.calories || 0) },
-      { name: 'Duration', data: result.map(activity => activity.duration || 0) },
-      { name: 'Distance', data: result.map(activity => activity.distance || 0) }
+      { name: 'Calories', data: result.map((activity) => activity.calories || 0) },
+      { name: 'Duration', data: result.map((activity) => activity.duration || 0) },
+      { name: 'Distance', data: result.map((activity) => activity.distance || 0) },
     ];
 
     const data = {
       date_range: 'Weekly',
-      series: series
+      series: series,
     };
 
     res.status(200).send({
@@ -310,8 +347,188 @@ const getGraphSummaryDataByUserId = async (req, res) => {
 //   }
 // };
 
+//try it out!!
+// const getsummaryForLast7Days = async (req, res) => {
+//   try {
+
+//     const date_range = req.body;
+//     // Calculate startDate and endDate
+//     const endDate = new Date(); // Today
+//     const startDate = new Date();
+
+//     if(date_range === 'today'){
+//       startDate.setDate(startDate.getDate()); // today
+//     }else if(date_range === 'weekly'){
+//       startDate.setDate(startDate.getDate() - 6); // 6 days ago
+//     }else if (date_range === 'monthly'){
+//       startDate.setDate(startDate.getDate() - 30); // 30 days ago
+//     }else if (date_range === 'yearly'){
+//       startDate.setDate(startDate.getDate() - 365); // 365 days ago
+//     }
+
+//     const result = await userModel.aggregate([
+//       {
+//         $match: { _id: new mongoose.Types.ObjectId(user_id) }, // Use the user_id from request params
+//       },
+//       {
+//         $lookup: {
+//           from: 'exercise-activities',
+//           localField: 'exercise_activities._id',
+//           foreignField: '_id',
+//           as: 'activities',
+//         },
+//       },
+//       {
+//         $unwind: '$activities',
+//       },
+//       {
+//         $match: { 'activities.date': { $gte: startDate, $lte: endDate } }, // Filter activities within the specified date range
+//       },
+//       {
+//         $group: {
+//           _id: '$_id',
+//           totalHours: { $sum: '$activities.hour' },
+//           totalMinutes: { $sum: '$activities.minute' },
+//           totalDistance: { $sum: '$activities.distance' },
+//           totalCalories: { $sum: '$activities.calories' },
+//         },
+//       },
+//       {
+//         $project: {
+//           _id: 0,
+//           user_id: '$_id',
+//           totalHours: 1,
+//           totalMinutes: 1,
+//           totalDistance: 1,
+//           totalCalories: 1,
+//         },
+//       },
+//     ]);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       success: false,
+//       message: 'Internal server error',
+//       error: error.message,
+//     });
+//   }
+// };
+
+const getdataByDateRange = async (req, res) => {
+  try {
+    const { user_id } = req.param;
+    let startDate, endDate;
+
+    // กำหนดช่วงเวลาตามความต้องการ
+    const { date_range } = req.query;
+    switch (date_range) {
+      case 'today':
+        // ช่วงรายชั่วโมง
+        startDate = new Date();
+        endDate = new Date();
+        startDate.setHours(startDate.getHours() - 1);
+        break;
+      case 'weekly':
+        // ช่วงย้อนหลัง 7 วัน
+        endDate = new Date();
+        startDate = new Date(endDate);
+        startDate.setDate(startDate.getDate() - 6);
+        break;
+      case 'monthly':
+        // ช่วงรายเดือน
+        const today = new Date();
+        startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        break;
+      case 'yearly':
+        // ช่วงรายปี
+        const currentYear = new Date().getFullYear();
+        startDate = new Date(currentYear, 0, 1);
+        endDate = new Date(currentYear, 11, 31);
+        break;
+      default:
+        // ค่าเริ่มต้นหากไม่ระบุ range
+        startDate = null;
+        endDate = null;
+    }
+
+    const result = await userModel.aggregate([
+      {
+        $match: { _id: new mongoose.Types.ObjectId(user_id) }, // Use the user_id from request params
+      },
+      {
+        $lookup: {
+          from: 'exercise-activities',
+          localField: 'exercise_activities._id',
+          foreignField: '_id',
+          as: 'activities',
+        },
+      },
+      {
+        $unwind: '$activities',
+      },
+      {
+        $match: { 'activities.date': { $gte: startDate, $lte: endDate } }, // Filter activities within the specified date range
+      },
+      {
+        $group: {
+          _id: '$_id',
+          totalHours: { $sum: '$activities.hour' },
+          totalMinutes: { $sum: '$activities.minute' },
+          totalDistance: { $sum: '$activities.distance' },
+          totalCalories: { $sum: '$activities.calories' },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          user_id: '$_id',
+          totalHours: 1,
+          totalMinutes: 1,
+          totalDistance: 1,
+          totalCalories: 1,
+        },
+      },
+    ]);
+
+    // Calculate total hours and minutes
+    let totalHours = 0;
+    let totalMinutes = 0;
+    let totalDistance = 0;
+    let totalCalories = 0;
+
+    result.forEach((item) => {
+      totalHours += item.totalHours;
+      totalMinutes += item.totalMinutes;
+      totalDistance += item.totalDistance;
+      totalCalories += item.totalCalories;
+    });
+    // Convert excess minutes to hours
+    const sumData = {
+      totalHours: (totalHours += Math.floor(totalMinutes / 60)),
+      totalMinutes: (totalMinutes %= 60),
+      totalDistance: result[0].totalDistance,
+      totalCalories: result[0].totalCalories,
+    };
+
+    res.status(200).json({
+      success: true,
+      message: 'Summary retrieved successfully',
+      data: sumData,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error: error.message,
+    });
+  }
+};
+
 export default {
   getsummaryCardByUserId,
   getActivitiesTypeByUserId,
   getGraphSummaryDataByUserId,
+  getdataByDateRange,
 };
