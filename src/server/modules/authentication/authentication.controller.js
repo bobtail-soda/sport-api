482e71fc8bd4b6cddac308d83ec03c2cf554bf54
Amornrat <Amornrat.Rtn@gmail.com>
2024-02-19 01:09:13 +0700

    change-signup

--- src/server/modules/authentication/authentication.controller.js
+++ src/server/modules/authentication/authentication.controller.js
@@ -1,6 +1,6 @@
+import bcrypt from 'bcrypt';
 import nodemailer from 'nodemailer';
 import { config } from '../../config/config.js';
-import bcrypt from 'bcrypt';
 import { createJwt } from '../../utils/createJwt.js';
 
 import userController from '../users/user.controller.js';
@@ -94,9 +94,11 @@ const signup = async (req, res) => {
         verificationCode: code,
       }); // Save code to database
 
-      user.password = undefined;
-      // step6: Send email with verification code
       await sendVerificationEmail(email, code);
+      // user = await userModel.create({ userName, email, password: hashedPassword, phone });
+      user.password = undefined;
+      user.verificationCode = undefined;
+      // step6: Send email with verification code
       console.log('Verification code sent to your email.');
 
       console.log('create user success');
